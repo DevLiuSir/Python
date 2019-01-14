e5a1a6d5e15440313a74a249ac37cb638f710498
ChinaHackers <93428739@qq.com>
2019-01-14 19:17:09 +0800

    Update MultiplicationFormulaTable.py

--- Example/MultiplicationFormulaTable.py
+++ Example/MultiplicationFormulaTable.py
@@ -1,12 +1,19 @@
 
 # 打印九九乘法表
 
+# ------------for循环九九乘法表------------
 for i in range(1, 10):
-
     for j in range(1, i + 1):
+        print('{} * {} = {}\t'.format(j, i, i * j), end="")
+    print('\n')
 
-        #print('{}x{}={}\t'.format(j, i, i * j), end="")
 
-        print('%d x %d = %d \t' %(i, j, i * j), end="")
-
-    print()
+# ------------while循环九九乘法表-----------
+i = 1
+while i < 10:
+    j = 1
+    while j < i + 1:
+        print(f'{j} * {i} = {i * j}\t', end='')
+        j += 1
+    print()     # print函数默认换行
+    i += 1
