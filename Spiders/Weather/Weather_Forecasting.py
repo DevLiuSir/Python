80ee9c120f00ef66e77365c925e1055c0cbc1adc
ChinaHackers <93428739@qq.com>
2019-01-14 19:12:44 +0800

    Update Weather_Forecasting.py

--- Spiders/Weather/Weather_Forecasting.py
+++ Spiders/Weather/Weather_Forecasting.py
@@ -15,26 +15,35 @@ import re
 url = 'http://p.weather.com.cn/2017/06/2720826.shtml#p=7'
 
 
-# 自定义下载页面函数
 def load_page(url):
+    '''
+    根据URL加载页面
+    :param url: 网页地址
+    :return:
+    '''
     response = requests.get(url)
     data = response.content
     return data
 
 
-# 自定义保存页面图片函数
 def get_image(html):
+    '''
+    根据URL地址获取图片数据
+    :param html:
+    :return:
+    '''
     regx = r'http://[\S]*jpg'  # 定义图片正则表达式
     pattern = re.compile(regx)  # 编译表达式构造匹配模式
-    get_images = re.findall(pattern, repr(html))  # 在页面中匹配图片链接
+    get_images = re.findall(pattern, repr(html))  # 在页面中匹配图片链接, get_images 是列表
 
     num = 1
     # 遍历匹配成功的链接
     for img in get_images:
+
         image = load_page(img)  # 根据图片链接，下载图片链接
 
         # 将下载的图片保存到对应的文件夹中
-        with open('./spider_picture/%s.jpg' % num, 'wb') as fb:
+        with open('./Weather/%s.jpg' % num, 'wb') as fb:
             fb.write(image)
             print("正在下载第%s张图片" % num)
             num = num + 1
@@ -42,9 +51,7 @@ def get_image(html):
     print("下载完成！")
 
 
-# 调用load_page函数，下载页面内容
-html = load_page(url)
-
 if __name__ == '__main__':
-    # 在页面中，匹配图片链接，并将图片下载下来，保存到对应文件夹
+
+    html = load_page(url)
     get_image(html)
