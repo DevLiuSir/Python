1507c075424ca1187f12da2a14ace2869a240dd0
ChinaHackers <93428739@qq.com>
2019-01-23 10:50:46 +0800

    Create random_pick.py

---
+++ Example/Lottery/random_pick.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+# Author: liuchuan
+# Contact: liuchuan910927@gmail.com
+# File: random_pick.py
+# Datetime: 2019-09-29 22:24
+# Software: PyCharm
+
+
+
+# ================= 双色球随机选号 ==============
+
+
+
+from random import randrange, randint, sample
+
+
+def display(balls):
+    """
+    输出列表中的双色球号码
+    """
+    for index, ball in enumerate(balls):
+        if index == len(balls) - 1:
+            print('|', end=' ')
+        print('%02d' % ball, end=' ')
+    print()
+
+
+def random_select():
+    """
+    随机选择一组号码
+    """
+    red_balls = [x for x in range(1, 34)]
+    selected_balls = []
+    for _ in range(6):
+        index = randrange(len(red_balls))
+        selected_balls.append(red_balls[index])
+        del red_balls[index]
+    # 上面的for循环也可以写成下面这行代码
+    # sample函数是random模块下的函数
+    # selected_balls = sample(red_balls, 6)
+    selected_balls.sort()
+    selected_balls.append(randint(1, 16))
+    return selected_balls
+
+
+def main():
+    n = int(input('机选几注双色球: '))
+    for _ in range(n):
+        display(random_select())
+
+
+if __name__ == '__main__':
+    main()
