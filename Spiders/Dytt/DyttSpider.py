af191452a9d166329cdbe91c1f485d31b6d9c750
ChinaHackers <93428739@qq.com>
2019-08-20 01:04:00 +0800

    Modifying the project structure.

--- Spiders/Dytt/DyttSpider.py
+++ Spiders/Dytt/DyttSpider.py
@@ -1,43 +1,55 @@
-#!/usr/bin/env python
+#!/usr/local/bin/python3
 #-*- coding:utf-8 -*-
 # Author: liuchuan
-# File: Dytt.py
-# Datetime: 2019-07-08 18:27
+# Contact: liuchuan910927@gmail.com
+# File: DyttSpider.py
+# Datetime: 2018/12/25 23:16
 # Software: PyCharm
 
 
-
-import requests     # 网络请求模块
-import re           # 提取数据
-import time         # time.sleep()
+import requests
+import re
 from lxml import etree
+from multiprocessing import Pool
 
-# 电影天堂:     https://www.dytt8.net/
 
 
 # 爬虫类
-class Spider(object):
+class DyttSpider:
+
+
+    def __init__(self):
+        # 请求头
+        self.headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
+
+
+    def get_all_htmls(self, max_num):
+        '''
+        下载所有列表页面的HTML，用于后续的分析
+        :return: 所有URL
+        '''
+        htmls = []  # 网址list，用于保存返回到所有网址
+
+        # 构造分页数字列表, 循环获取所有网址
+        page_indexs = range(1, max_num + 1)
+        for idx in page_indexs:
+            url = 'https://www.dytt8.net/html/gndy/dyzz/list_23_' + str(idx) + '.html'
+            htmls.append(url)
+        return htmls
 
 
     def load_webpage_data(self, url):
         """
         加载指定url页面的内容
         :param url: 链接
-        :return:
+        :return: 网页源码
         """
-        Headers = {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
-        }
-
-        response = requests.get(url=url, headers=Headers)
+        response = requests.get(url=url, headers=self.headers)
         response.encoding = 'gb2312'        # 编码
-        print("状态码:", (response.status_code))
-        html = response.text
-
-        # r.raise_for_status()  #如果状态不是200，则引发异常
+        # print("状态码:", (response.status_code))
         try:
             if response.status_code == 200:  # 如果状态码为：200，访问成功，返回网页源码，否返回none
-                return html
+                return response.text
             return None
         except requests.ConnectTimeout:
             print('连接远程服务器超时异常！')
@@ -46,8 +58,8 @@ class Spider(object):
     def parse_webpage(self, html):
         """
         筛选html内容,返回列表
-        :param html:
-        :return:
+        :param html: 需要解析的URL
+        :return: 电影链接列表
         """
 
         # re.compile: 编译表达式构造匹配模式
@@ -67,41 +79,40 @@ class Spider(object):
         return item_list
 
 
-    def join_movies_detail_url(self, item_list):
+    def get_ftp_url(self, item_list):
         '''
-        拼接电影详情页码的url
+        获取ftp下载链接
         :param item_list: 匹配的电影链接列表
-        :return:
+        :return: 筛选后的ftp下载链接
         '''
-
+        # 1.遍历电影链接列表
         for li in item_list:
 
-            # 拼接电影详情页码的url
+            # 1.1.拼接电影详情页码的url
             base_url = 'https://www.dytt8.net'+li[0]
 
-            # 电影详情页面
+            # 2. 加载电影详情页面
             movie_details_url = self.load_webpage_data(base_url)
 
-            regx2 = r'<td style="WORD-WRAP: break-word" bgcolor="#fdfddf"><a href="(.*?)">.*?</a></td>'
-
+            # 3.清洗数据
+            regx2 = r'<td style="WORD-WRAP: break-word" bgcolor="#fdfddf"><a href=".*?">(.*?)</a></td>'
             pattern2 = re.compile(regx2)
-
             # 文档传输协议
             ftp = re.findall(pattern2, movie_details_url)
 
-            self.write_text(ftp)
+            return ftp
+
 
 
     def write_text(self, list):
         """
         写入文本. 已追加的形式,存储筛选后的内容
         :param list: 筛选后的数据, 列表形式
-        :return:
         """
         # 遍历匹配成功的链接
         for content in list:
 
-            print(content)
+            # print(content)
 
             '''
             # 关于open()的mode参数：
@@ -114,10 +125,10 @@ class Spider(object):
             # 对应的，如果是二进制文件，就都加一个b就好啦：
             # 'rb'　　'wb'　　'ab'　　'rb+'　　'wb+'　　'ab+'
             '''
-            with open("/Users/liuchuan/Desktop/dytt.text", 'a+', encoding='utf-8') as f:
-
-                # write: 写入本地, write 只能写入文本
-                f.write(content+"\n")
+            with open("/Users/liuchuan/Desktop/dytt.txt", 'a+', encoding='utf-8') as f:
+                print(f"[INFO]: 已保存: {content}")
+                f.write(content+"\n")           # write: 写入本地, write 只能写入文本
+                f.close()
 
 
     def get_max_page_number(self):
@@ -147,24 +158,30 @@ class Spider(object):
 
 if __name__ == '__main__':
 
+    print('*' * 32)
+    print('*' * 10 + ' 电影天堂爬虫 ' + '*' * 10)
+    print('*' * 32)
+
     # 实例化类对象
-    my_spider = Spider()
+    my_spider = DyttSpider()
 
     # 最大页码
     max_number = my_spider.get_max_page_number()
 
-    # print(max_number)
+    # 1.获取所有的网址
+    all_html = my_spider.get_all_htmls(max_number)
 
-    for n in range(1, max_number + 1):      # 循环获取所有网址
+    # 1.1.遍历网址
+    for html in all_html:
 
-        # 1.获取所有的网址
-        url = 'https://www.dytt8.net/html/gndy/dyzz/list_23_'+str(n)+'.html'
-
-        # 2.根据网址，下载页面内容
-        html = my_spider.load_webpage_data(url)
+        # 2.根据网址，加载页面内容
+        webpage_source = my_spider.load_webpage_data(html)
 
         # 3.根据html，筛选数据
-        item_list = my_spider.parse_webpage(html)
+        item_list = my_spider.parse_webpage(webpage_source)
 
         # 4.拼接url
-        my_spider.join_movies_detail_url(item_list)
+        ftp = my_spider.get_ftp_url(item_list)
+
+        # 5.写入文本
+        my_spider.write_text(ftp)
